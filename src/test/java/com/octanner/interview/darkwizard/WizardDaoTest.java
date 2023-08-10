736956be928e7e57d98c119523e385d305a1cc8a
Christian Rigdon <christian.rigdon@octanner.com>
2023-08-10 17:02:20 -0600

    Add a few tests and check for equals/hashCode

--- src/test/java/com/octanner/interview/darkwizard/WizardDaoTest.java
+++ src/test/java/com/octanner/interview/darkwizard/WizardDaoTest.java
@@ -26,7 +26,32 @@ class WizardDaoTest {
   }
 
   @Test
-  @DisplayName("testing the order of results")
+  @DisplayName("Tom Riddle should be listed first and have cast 16 spells")
+  public void testTomRiddle() {
+    var darkWizards = wizardDao.getDarkWizards();
+    var tom = darkWizards.get(0);
+    assertThat(tom.wizardName()).isEqualTo("Tom Riddle");
+    assertThat(tom.spellCastCount()).isEqualTo(16);
+  }
+
+  @Test
+  @DisplayName("Harry Potter should be listed 4th and have cast 2 spells")
+  public void testHarryPotter() {
+    var darkWizards = wizardDao.getDarkWizards();
+    var harry = darkWizards.get(3);
+    assertThat(harry.wizardName()).isEqualTo("Harry Potter");
+    assertThat(harry.spellCastCount()).isEqualTo(2);
+  }
+
+  @Test
+  @DisplayName("Hermione Granger should not be in the list")
+  public void testHermioneGranger() {
+    var darkWizards = wizardDao.getDarkWizards();
+    assertThat(darkWizards.stream().anyMatch(w -> w.wizardName().equals("Hermione Granger"))).isFalse();
+  }
+
+  @Test
+  @DisplayName("Testing if you can compare lists of objects")
   public void testTomIsFirst() {
     var expected = List.of(
         new WizardResultData("Tom Riddle", 16),
@@ -35,6 +60,7 @@ class WizardDaoTest {
         new WizardResultData("Harry Potter", 2),
         new WizardResultData("Draco Malfoy", 1)
     );
-    assertThat(wizardDao.getDarkWizards()).containsExactlyElementsOf(expected);
+    var darkWizards = wizardDao.getDarkWizards();
+    assertThat(darkWizards).containsExactlyElementsOf(expected);
   }
 }
